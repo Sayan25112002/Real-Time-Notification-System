4699d5efc478fb822fe86920817e01f0d18453c4
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-09 13:58:50 +0530

    NotificationRequestDto Added and Modified

--- src/main/java/com/RealTime/NotificationSystem/dto/requestDto/NotificationRequestDto.java
+++ src/main/java/com/RealTime/NotificationSystem/dto/requestDto/NotificationRequestDto.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class MessageRequestDto {
+public class NotificationRequestDto {
 
     private String message;
 
---
+++ src/main/java/com/RealTime/NotificationSystem/entity/Message.java
@@ -0,0 +1,4 @@
+package com.RealTime.NotificationSystem.entity;
+
+public class Message {
+}
