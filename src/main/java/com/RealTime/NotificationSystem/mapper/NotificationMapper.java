334db6aabacff01c1bf7be5195dfea46fcc5096b
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-09 14:05:26 +0530

    NotificationMapper Added and Modified

---
+++ src/main/java/com/RealTime/NotificationSystem/mapper/NotificationMapper.java
@@ -0,0 +1,19 @@
+package com.RealTime.NotificationSystem.mapper;
+
+import com.RealTime.NotificationSystem.dto.requestDto.NotificationRequestDto;
+import com.RealTime.NotificationSystem.dto.responseDto.NotificationResponseDto;
+import com.RealTime.NotificationSystem.entity.Notification;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface NotificationMapper {
+
+    Notification toNotification(NotificationRequestDto notificationRequestDto);
+
+    NotificationResponseDto toNotificationResponseDto(Notification notification);
+
+    List<NotificationResponseDto> toNotificationResponseDto(List<Notification> notifications);
+
+}
