bad835b90a8247a41a3352d202111d9698968d2d
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-09 14:06:11 +0530

    NotificationMapper Modified

--- src/main/java/com/RealTime/NotificationSystem/mapper/NotificationMapper.java
+++ src/main/java/com/RealTime/NotificationSystem/mapper/NotificationMapper.java
@@ -7,7 +7,7 @@ import org.mapstruct.Mapper;
 
 import java.util.List;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface NotificationMapper {
 
     Notification toNotification(NotificationRequestDto notificationRequestDto);
