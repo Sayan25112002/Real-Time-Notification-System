7d592f90cca8aba10b260b82c6c83da02c9bb74f
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-09 14:00:09 +0530

    NotificationResponseDto Added and Modified

--- src/main/java/com/RealTime/NotificationSystem/dto/responseDto/NotificationResponseDto.java
+++ src/main/java/com/RealTime/NotificationSystem/dto/responseDto/NotificationResponseDto.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class MessageResponseDto {
+public class NotificationResponseDto {
 
     private Long id;
 
