904e5222508b4166c5eebc78d2d0f739d97865a7
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-09 15:03:10 +0530

    NotificationController Modified

--- src/main/java/com/RealTime/NotificationSystem/controller/NotificationController.java
+++ src/main/java/com/RealTime/NotificationSystem/controller/NotificationController.java
@@ -4,9 +4,12 @@ import com.RealTime.NotificationSystem.dto.requestDto.NotificationRequestDto;
 import com.RealTime.NotificationSystem.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,7 +19,11 @@ public class NotificationController {
 
     @MessageMapping("/sendMessage")
     @SendTo("/topic/notifications")
-    public String sendMessage(@RequestBody NotificationRequestDto notificationRequestDto){
+    public String sendMessage(String message){
+        NotificationRequestDto notificationRequestDto = NotificationRequestDto.builder()
+                .message(message)
+                .messageSendAt(LocalDateTime.now())
+                .build();
         return notificationService.sendNotification(notificationRequestDto);
     }
 }
