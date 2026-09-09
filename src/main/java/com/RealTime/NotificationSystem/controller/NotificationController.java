d6e1bbb8162fab050f5ee4c5e2c2290debbdd273
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-09 14:41:50 +0530

    NotificationController.java Modified

--- src/main/java/com/RealTime/NotificationSystem/controller/NotificationController.java
+++ src/main/java/com/RealTime/NotificationSystem/controller/NotificationController.java
@@ -1,17 +1,22 @@
 package com.RealTime.NotificationSystem.controller;
 
+import com.RealTime.NotificationSystem.dto.requestDto.NotificationRequestDto;
+import com.RealTime.NotificationSystem.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class NotificationController {
 
+    private final NotificationService notificationService;
+
     @MessageMapping("/sendMessage")
     @SendTo("/topic/notifications")
-    public String sendMessage(String message){
-        return message;
+    public String sendMessage(@RequestBody NotificationRequestDto notificationRequestDto){
+        return notificationService.sendNotification(notificationRequestDto);
     }
 }
